6b21ea5914f034cf8b462281232372886526e44c
Zacharias Levander <zacharias.levander@mafy.fi>
2020-12-26 09:29:59 +0200

    Add tests for the login flow
    This marks my answer for exercise 5.18.

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -17,4 +17,20 @@ describe('Blog app', function() {
     cy.contains('username')
     cy.contains('password')
   })
+
+  describe('Login',function() {
+    it('should succeed with correct credentials', function() {
+      cy.get('#username').type(username)
+      cy.get('#password').type(password)
+      cy.get('#login-button').click()
+      cy.contains(`${name} is logged in.`)
+    })
+
+    it('should fail with wrong credentials', function() {
+      cy.get('#username').type('wrongusername')
+      cy.get('#password').type('wrongpassword')
+      cy.get('#login-button').click()
+      cy.contains('Failed to login!')
+    })
+  })
 })
